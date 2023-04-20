3e27f5c386187b954974e65f050169b77786a61c
Owinnie <tillawinnie@gmail.com>
2023-04-20 11:16:17 +0300

    SmG

---
+++ uploads_compressed/home.js
@@ -0,0 +1,1 @@
+const toggleBtn=document.querySelector(".toggleBtn");const toggleBtnIcon=document.querySelector(".toggleBtn i");const dropdownMenu=document.querySelector(".dropdownMenu");toggleBtn.onclick=function(){dropdownMenu.classList.toggle("open");const a=dropdownMenu.classList.contains("open");toggleBtnIcon.classList=a?"fa-solid fa-xmark":"fa-solid fa-bars"};
